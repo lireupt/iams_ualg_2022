9596807abd019f3dc9e6a926bc07dc85e38ee612
Luckyb@no <59529474+luckybano@users.noreply.github.com>
2022-11-27 05:15:13 +0000

    Update vmwareTest.ps1

--- vmwareTest.ps1
+++ vmwareTest.ps1
@@ -76,6 +76,8 @@ echo $SID $userPrincipalName $sAMAccountName $snName $givenName $userPassword $u
 #New-ADOrganizationalUnit -name "test1" -Path "OU=test_group7,OU=Exercicios,OU=User Accounts,DC=cs,DC=local"
 New-ADOrganizationalUnit -name $department -Path "OU=test_group7,OU=Exercicios,OU=User Accounts,DC=cs,DC=local"
 
+#Proteção Eliminar acidentalmente
+ProtectedFromAccidentalDeletion $false
 
 
 New-ADUser  -name 'Helder'` #ok
@@ -108,5 +110,26 @@ New-ADUser -Name $sn -SamAccountName $sAMAccountName  `
 			#-ObjectGUID $ObjectGUID  
 
 
-            
+ ### Para Rever 
+ #Create administrative groups
+ 
+ 
+$adm_grp=New-ADGroup ($City+ "_admins") -path ("OU=Admins,OU="+$CityFull+","+$ParentOU) -GroupScope Global -PassThru –Verbose
+$adm_wks=New-ADGroup ($City+ "_account_managers") -path ("OU=Admins,OU="+$CityFull+","+$ParentOU) -GroupScope Global -PassThru –Verbose
+$adm_account=New-ADGroup ($City+ "_wks_admins") -path ("OU=Admins,OU="+$CityFull+","+$ParentOU) -GroupScope Global -PassThru –Verbose
+##### An example of assigning password reset permissions for the _account_managers group on the Users OU
+$confADRight = "ExtendedRight"
+$confDelegatedObjectType = "bf967aba-0de6-11d0-a285-00aa003049e2" # User Object Type GUID
+$confExtendedRight = "00299570-246d-11d0-a768-00aa006e0529" # Extended Right PasswordReset GUID
+$acl=get-acl ("AD:OU=Users,OU="+$CityFull+","+$ParentOU)
+$adm_accountSID = [System.Security.Principal.SecurityIdentifier]$adm_account.SID
+#Build an Access Control Entry (ACE)string
+$aceIdentity = [System.Security.Principal.IdentityReference] $adm_accountSID
+$aceADRight = [System.DirectoryServices.ActiveDirectoryRights] $confADRight
+$aceType = [System.Security.AccessControl.AccessControlType] "Allow"
+$aceInheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "Descendents"
+$ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule($aceIdentity, $aceADRight, $aceType, $confExtendedRight, $aceInheritanceType,$confDelegatedObjectType)
+# Apply ACL
+$acl.AddAccessRule($ace)
+Set-Acl -Path ("AD:OU=Users,OU="+$CityFull+","+$ParentOU) -AclObject $acl
 
